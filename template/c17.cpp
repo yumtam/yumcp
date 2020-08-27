97d7d1b7c22ef1ffc6eecfd252c15900b1f95f9c
yumtam <tmp_0@naver.com>
2020-08-27 21:11:12 +0900

    add c17 template

---
+++ template/c17.cpp
@@ -0,0 +1,19 @@
+//   Author: yumtam
+//   Created at: ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+#define rep(i, n) for (int i=0; i<(n); ++i)
+#define rep2(i, s, e, d) for (int i=(s); (d)>0?i<(e):i>(e); i+=(d))
+#define all(c) (c).begin(), (c).end()
+#define sz(v) (static_cast<int>(v.size()))
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #[[$END$]]#
+}
